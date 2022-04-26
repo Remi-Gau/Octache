f3bac6984f292b052341244282b21ba0b225d722
Remi Gau <remi_gau@hotmail.com>
2022-04-26 09:26:31 +0200

    lint

--- tests/test_get_key.m
+++ tests/test_get_key.m
@@ -12,75 +12,75 @@ function test_suite = test_get_key %#ok<*STOUT>
 end
 
 function test_get_key_dot()
-  
-  % GIVEN
-  warn = false;
-  keep = false;
-  l_del = '{{';
-  r_del = '}}';
-  key = '.';
-  scopes = {struct('foo', 1);
-            struct('bar', 2)};
-          
-          % WHEN
-  value = get_key(key, scopes, warn, keep, l_del, r_del);
-  
+
+    % GIVEN
+    warn = false;
+    keep = false;
+    l_del = '{{';
+    r_del = '}}';
+    key = '.';
+    scopes = {struct('foo', 1)
+              struct('bar', 2)};
+
+    % WHEN
+    value = get_key(key, scopes, warn, keep, l_del, r_del);
+
     % THEN
-  assertEqual(value,  struct('foo', 1))
-  
+    assertEqual(value,  struct('foo', 1));
+
 end
 
 function test_get_key_value()
-  
-  % GIVEN
-  warn = false;
-  keep = false;
-  l_del = '{{';
-  r_del = '}}';
-  key = 'bar';
-  scopes = {struct('foo', 1);
-            struct('bar', 2)};
-          
-          % WHEN
-  value = get_key(key, scopes, warn, keep, l_del, r_del);
-  
+
+    % GIVEN
+    warn = false;
+    keep = false;
+    l_del = '{{';
+    r_del = '}}';
+    key = 'bar';
+    scopes = {struct('foo', 1)
+              struct('bar', 2)};
+
+    % WHEN
+    value = get_key(key, scopes, warn, keep, l_del, r_del);
+
     % THEN
-  assertEqual(value,  2)
-  
+    assertEqual(value,  2);
+
 end
 
 function test_get_key_value_nested()
-  
-  % GIVEN
-  warn = false;
-  keep = false;
-  l_del = '{{';
-  r_del = '}}';
-  key = 'for.bar.baz.foz';
-  scopes = {struct('for', struct('bar', struct('baz', struct('foz', 2))))};
-  
-  % WHEN
-  value = get_key(key, scopes, warn, keep, l_del, r_del);
-  
-  % THEN
-  assertEqual(value,  2)
-  
+
+    % GIVEN
+    warn = false;
+    keep = false;
+    l_del = '{{';
+    r_del = '}}';
+    key = 'for.bar.baz.foz';
+    scopes = {struct('for', struct('bar', struct('baz', struct('foz', 2))))};
+
+    % WHEN
+    value = get_key(key, scopes, warn, keep, l_del, r_del);
+
+    % THEN
+    assertEqual(value,  2);
+
 end
 
 function test_get_key_value_missing()
-  
-  % GIVEN
-  warn = false;
-  keep = false;
-  l_del = '{{';
-  r_del = '}}';
-  key = 'for.bar.baz.foo';
-  scopes = {struct('for', struct('bar', struct('baz', struct('foz', 2))))};
-  
-  % WHEN
-  value = get_key(key, scopes, warn, keep, l_del, r_del);
-  
-  % THEN
-  assertEqual(value,  '')
-  
+
+    % GIVEN
+    warn = false;
+    keep = false;
+    l_del = '{{';
+    r_del = '}}';
+    key = 'for.bar.baz.foo';
+    scopes = {struct('for', struct('bar', struct('baz', struct('foz', 2))))};
+
+    % WHEN
+    value = get_key(key, scopes, warn, keep, l_del, r_del);
+
+    % THEN
+    assertEqual(value,  '');
+
 end
